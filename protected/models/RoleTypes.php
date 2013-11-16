528b6ea58c5a991e3bf84e9446f3f290117009f0
Edbert Guinto <ecg_24k@yahoo.com>
2013-11-16 12:07:20 +0800

    new models

---
+++ protected/models/RoleTypes.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * This is the model class for table "role_types".
+ *
+ * The followings are the available columns in table 'role_types':
+ * @property integer $id
+ * @property string $role_name
+ * @property integer $control
+ */
+class RoleTypes extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return RoleTypes the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'role_types';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('id, role_name, control', 'required'),
+			array('id, control', 'numerical', 'integerOnly'=>true),
+			array('role_name', 'length', 'max'=>50),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('id, role_name, control', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'role_name' => 'Role Name',
+			'control' => 'Control',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('role_name',$this->role_name,true);
+		$criteria->compare('control',$this->control);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
