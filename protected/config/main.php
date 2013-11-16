5f09915542f77d27256197b610b129e2fa9f5ee3
junereycasuga <junerey.casuga@gmail.com>
2013-11-16 16:17:37 +0800

    deleted unnecessary files

--- protected/config/main.php
+++ protected/config/main.php
@@ -54,13 +54,13 @@ return array(
 		// ),
 		// uncomment the following to use a MySQL database
 		
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=watchout',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
+		// 'db'=>array(
+		// 	'connectionString' => 'mysql:host=localhost;dbname=watchout',
+		// 	'emulatePrepare' => true,
+		// 	'username' => 'root',
+		// 	'password' => '',
+		// 	'charset' => 'utf8',
+		// ),
 		
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
