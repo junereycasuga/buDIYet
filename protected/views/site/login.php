412009fba48881ad630796c9739c14fd2325c29c
junereycasuga <junerey.casuga@gmail.com>
2013-11-16 13:55:14 +0800

    update on registration and login layout

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -21,28 +21,32 @@ $this->breadcrumbs=array(
 		'validateOnSubmit'=>true,
 	),
 )); ?>
-
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
-
+	
 	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username'); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
+		<div class="well col-md-5 col-sm-10">
+			<p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
-		<p class="hint">
-			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
-		</p>
-	</div>
+			<div class="form-group">
+				<?php echo $form->labelEx($model,'username'); ?>
+				<?php echo $form->textField($model,'username', array('class'=>'form-control')); ?>
+				<?php echo $form->error($model,'username'); ?>
+			</div>
 
-	<!--  -->
+			<div class="form-group">
+				<?php echo $form->labelEx($model,'password'); ?>
+				<?php echo $form->passwordField($model,'password', array('class'=>'form-control')); ?>
+				<?php echo $form->error($model,'password'); ?>
+				<p class="hint">
+					Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
+				</p>
+			</div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Login'); ?>
+			<!--  -->
+
+			<div class="form-group buttons">
+				<?php echo CHtml::submitButton('Login', array('class'=>'btn btn-primary')); ?>
+			</div>
+		</div>
 	</div>
 
 <?php $this->endWidget(); ?>
--- protected/views/site/register.php
+++ protected/views/site/register.php
@@ -5,39 +5,42 @@
 		'validateOnSubmit'=>true,
 	),
 )); ?>
-	
 	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username'); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
+		<div class="well col-md-5 col-sm-10">
+			<div class="form-group">
+				<?php echo $form->labelEx($model,'username'); ?>
+				<?php echo $form->textField($model,'username', array('class'=>'form-control')); ?>
+				<?php echo $form->error($model,'username'); ?>
+			</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
-	</div>
+			<div class="form-group">
+				<?php echo $form->labelEx($model,'password'); ?>
+				<?php echo $form->passwordField($model,'password', array('class'=>'form-control')); ?>
+				<?php echo $form->error($model,'password'); ?>
+			</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'first_name'); ?>
-		<?php echo $form->textField($model,'first_name'); ?>
-		<?php echo $form->error($model,'first_name'); ?>
-	</div>
+			<div class="form-group">
+				<?php echo $form->labelEx($model,'first_name'); ?>
+				<?php echo $form->textField($model,'first_name', array('class'=>'form-control')); ?>
+				<?php echo $form->error($model,'first_name'); ?>
+			</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'last_name'); ?>
-		<?php echo $form->textField($model,'last_name'); ?>
-		<?php echo $form->error($model,'last_name'); ?>
-	</div>
+			<div class="form-group">
+				<?php echo $form->labelEx($model,'last_name'); ?>
+				<?php echo $form->textField($model,'last_name', array('class'=>'form-control')); ?>
+				<?php echo $form->error($model,'last_name'); ?>
+			</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'email'); ?>
-		<?php echo $form->textField($model,'email'); ?>
-		<?php echo $form->error($model,'email'); ?>
-	</div>
+			<div class="form-group">
+				<?php echo $form->labelEx($model,'email'); ?>
+				<?php echo $form->textField($model,'email', array('class'=>'form-control')); ?>
+				<?php echo $form->error($model,'email'); ?>
+			</div>
 
-	<div class="row">
-		<?php echo CHtml::submitButton('Reigster',array('name'=>'btnRegister')); ?>
+			<div class="form-group">
+				<?php echo CHtml::submitButton('Reigster',array('name'=>'btnRegister', 'class'=>'btn btn-primary')); ?>
+			</div>
+		</div>
 	</div>
 
 <?php $this->endWidget(); ?>
