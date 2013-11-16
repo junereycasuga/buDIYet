1544b18a61501d9a3909f3e212b92ca15d341a27
junereycasuga <junerey.casuga@gmail.com>
2013-11-16 20:59:26 +0800

    updates on the database

--- DB/budiyet.sql
+++ DB/budiyet.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
+-- version 3.5.8.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 16, 2013 at 01:49 PM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
+-- Generation Time: Nov 16, 2013 at 08:58 PM
+-- Server version: 5.5.34-0ubuntu0.13.04.1
+-- PHP Version: 5.4.9-4ubuntu2.3
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `watchout`
+-- Database: `budiyet`
 --
 
 -- --------------------------------------------------------
@@ -78,6 +78,31 @@ INSERT INTO `diy_breakdown` (`diy_id`, `department`, `owner`, `project_name`, `b
 (11, 'CONGRESS', 'CA', 'Operations', 0, 2),
 (11, 'CONGRESS', 'HET', 'General Administration and Support', 0, 2),
 (11, 'CONGRESS', 'HET', 'General management and supervision', 1917000, 2),
+(11, 'CONGRESS', 'HET', 'Operations', 0, 2),
+(4, 'ARF', 'ARF', 'For the Requirements of the Comprehensive Agrarian Reform Program', 0, 1),
+(4, 'AFMF', 'NAFC-AFMF', 'Goat Production Project for the Accelerated Hunger Mitigation Program (AHMP-PL 480)', 0, 1),
+(4, 'CONGRESS', 'SET', 'For Adjudication of Electoral Contests Involving Members of the Senate Including Administrative Support', 3092000, 2),
+(5, 'AFMF', 'OSEC-DAR-AFM', 'Loan Proceeds', 593701000, 1),
+(5, 'AFMF', 'NAFC-AFMF', 'Goat Production Project for the Accelerated Hunger Mitigation Program (AHMP-PL 480)', 0, 1),
+(5, 'CONGRESS', 'SET', 'General management and supervision', 1571000, 2),
+(5, 'CONGRESS', 'HET', 'General Administration and Support Services', 0, 2),
+(6, 'AFMF', 'OSEC-DAR-AFM', 'Peso Counterpart', 149271000, 1),
+(6, 'AFMF', 'PRRI-AFMF', 'Open Academy for Philippine Agriculture (OPAPA).', 20000000, 1),
+(6, 'CONGRESS', 'HR', 'Operational Requirements of the Congressional Oversight Committee on Universally-Accessible Cheaper and Quality Medicines Act', 0, 2),
+(6, 'DA', 'BUPHIRE', 'Policy formulation and planning services', 464000, 2),
+(6, 'DAR', 'OSEC-DAR', 'Region VII', 617000, 2),
+(7, 'AFMF', 'OSEC-DAR-AFM', 'Tulay ng Pangulo Para Sa Kaunlarang Pang-Agraryo (TPKP) French Loan', 0, 1),
+(7, 'CONGRESS', 'SP', 'General Administration and Support', 0, 2),
+(7, 'CONGRESS', 'CA', 'General Administration and Support', 0, 2),
+(7, 'CONGRESS', 'HR', 'General Administration and Support Services', 0, 2),
+(11, 'ARF', 'ARF', 'For the Requirements of the Comprehensive Agrarian Reform Program', 0, 1),
+(11, 'AFMF', 'PCOA-AFMF', 'Participatory Coconut Planting Project', 100000000, 1),
+(11, 'AFMF', 'PRRI-AFMF', 'Locally-Funded Projects', 0, 1),
+(11, 'AFMF', 'PRRI-AFMF', 'Promotion of Palaycheck and Palayamanan', 48030000, 1),
+(11, 'CONGRESS', 'CA', 'General Administration and Support Services', 0, 2),
+(11, 'CONGRESS', 'CA', 'Operations', 0, 2),
+(11, 'CONGRESS', 'HET', 'General Administration and Support', 0, 2),
+(11, 'CONGRESS', 'HET', 'General management and supervision', 1917000, 2),
 (11, 'CONGRESS', 'HET', 'Operations', 0, 2);
 
 -- --------------------------------------------------------
@@ -108,6 +133,29 @@ INSERT INTO `diy_budgets` (`id`, `user_id`, `date_created`, `likes`, `dislikes`)
 (9, 1, '2013-11-16 12:48:34', 0, 0),
 (10, 1, '2013-11-16 12:48:56', 0, 0),
 (11, 1, '2013-11-16 12:49:14', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `full_name` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `full_name`, `email`) VALUES
+(1, 'junereycasuga', '0655a949c0826968e2daa324026f7b2dcccd94ac882ba5a4d5cec0fbc1fc0ff545a1a3b77560b7ead1704ca9f41b73a91c7d238f946f723e66fa96a17f1c3558', 'Junerey Casuga', 'junerey.casuga@gmail.com'),
+(4, 'test', 'ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff', 'Test User', 'test@email.com');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
