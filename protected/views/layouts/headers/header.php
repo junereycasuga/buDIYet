59ba47ee42817f0a16a65e4e762357818830c7ee
rienierpatron <rienierpatron@gmail.com>
2013-11-16 12:03:12 +0800

    added bootstrap

---
+++ protected/views/layouts/headers/header.php
@@ -0,0 +1,14 @@
+<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">WatchOut</a>
+		</div>
+		<div class="collapse navbar-collapse">
+		  <ul class="nav navbar-nav">
+		    <li><a href="#">Home</a></li>
+		    <li><a href="#about">About</a></li>
+		    <li><a href="#contact">Contact</a></li>
+		  </ul>
+		</div>
+	</div>
+</div>
