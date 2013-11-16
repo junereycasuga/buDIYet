1acf522ad75c0d4dc4623682f6d1b387ac43808d
Edbert Guinto <ecg_24k@yahoo.com>
2013-11-16 19:51:23 +0800

    new layout

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -12,17 +12,32 @@ $this->pageTitle=Yii::app()->name;
 	</div>
 	<div class="row">
 		<div class="col-md-6">
-			<div class="well well-sm">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu sollicitudin nunc. Aliquam mauris mi, vulputate vitae elementum quis, varius et mauris. Ut erat lacus, pretium vel diam iaculis, imperdiet congue mi. Aenean vel est lacus. Morbi fermentum dapibus velit. Praesent in dui lobortis, tincidunt enim id, pellentesque augue. Curabitur in nunc luctus, scelerisque diam non, lobortis nunc. Aenean eleifend, risus non lacinia interdum, nunc ipsum varius velit, eget varius mauris magna id augue.
-
-				Aliquam lacus lorem, euismod in urna non, faucibus aliquam sapien. Proin lacinia augue magna, sit amet accumsan velit interdum fermentum. Quisque placerat ultrices nisl, sed vehicula nisi porta sed. In placerat lectus sit amet nulla faucibus, ut gravida neque tincidunt. Pellentesque dignissim tristique justo et condimentum. Vestibulum eget iaculis dolor, hendrerit porttitor tellus. Aliquam dictum dui tortor, vel viverra urna dignissim eget. Vivamus ligula felis, suscipit ut dui non, vehicula pharetra nisl. Morbi ac ullamcorper sem, blandit congue est. Vestibulum a ipsum urna. Phasellus vitae rhoncus diam, sed accumsan dui.
-
-				Praesent sit amet egestas ipsum, at commodo leo. Cras hendrerit enim nibh, ut bibendum neque lobortis a. Etiam at hendrerit risus, ac fermentum libero. Ut placerat a nisi ac pulvinar. Phasellus elementum pharetra nunc, et lacinia tortor rhoncus at. Proin semper euismod gravida. Fusce rutrum ante eget fermentum aliquam.
+			<div>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
 			</div>
 		</div>
 		<div class="col-md-6">
-			<div class="well well-sm">
-				<a href="<?php echo $this->createAbsoluteUrl('department/'); ?>" class="btn btn-danger btn-lg">Do it yourself</a>
+			<div class="well">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu sollicitudin nunc. Aliquam mauris mi, vulputate vitae elementum quis, varius et mauris. Ut erat lacus, pretium vel diam iaculis, imperdiet congue mi. Aenean vel est lacus. Morbi fermentum dapibus velit. Praesent in dui lobortis, tincidunt enim id, pellentesque augue. Curabitur in nunc luctus, scelerisque diam non, lobortis nunc. Aenean eleifend, risus non lacinia interdum, nunc ipsum varius velit, eget varius mauris magna id augue.
+				</p>
+				<div class="row">
+					<div class="col-md-6">
+						<a href="<?php echo $this->createAbsoluteUrl('department/'); ?>" class="btn btn-danger btn-lg">Budget Your Own</a>
+					</div>
+					<div class="col-md-6">
+						<a href="<?php echo $this->createAbsoluteUrl('#'); ?>" class="btn btn-danger btn-lg"><i class="glyphicon glyphicon-thumbs-up"></i> Vote</a>
+					</div>
+				</div>
 			</div>	
 		</div>
 	</div>	
