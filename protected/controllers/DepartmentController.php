0d37d2df2ca5db2d568c6d2190d0c94e50719f16
rienierpatron <rienierpatron@gmail.com>
2013-11-16 20:06:14 +0800

    accordion

--- protected/controllers/DepartmentController.php
+++ protected/controllers/DepartmentController.php
@@ -2,6 +2,14 @@
 	class DepartmentController extends Controller {
 
 		public function actionBudget(){
+			if(isset($_POST['diy_submit'])){
+
+				$new_appro = $_POST['data_new_appro'];
+				$auto_appro = $_POST['data_auto_appro'];
+
+				$save_data = DiyBudgets::saveDIY(1,$new_appro,$auto_appro);
+				
+			}
 			$gaa = Curl::dataBudget(null,null,null,null,null);
 			$auto = $gaa->data{0}->auto_appro;
 			$new = $gaa->data{1}->new_appro;
--- protected/views/department/budget.php
+++ protected/views/department/budget.php
@@ -1,69 +1,95 @@
-<h3>New Appro</h3>
-<div class="panel-group" id="accordion1">
-	<?php foreach ($new_appro as $key => $code) { ?>
-		<div class="panel panel-default">
-		    <a data-toggle="collapse" data-parent="#accordion1" href="#collapse<?php echo $code->owner_code; ?>">
-		    <div class="panel-heading">
-		      <h4 class="panel-title">
-		        	
-					<b><?php  echo $code->department_desc." (".$code->department_code.")"; ?></b>
-			<!-- <td colspan=""><?php  echo $code->department_desc; ?> </td>  -->
-					 - <?php  echo $code->owner_code; ?>
-			<!-- <td colspan=""><?php  echo $code->owner_desc; ?> </td> --> 
-				
-		      </h4>
-		    </div>
-		    </a>
-		<?php if($code->programs) { ?>
-			<div id="collapse<?php echo $code->owner_code; ?>" class="panel-collapse collapse">
-      			<div class="panel-body">
-					<?php foreach ($code->programs as $key => $programs) { ?>
-						<?php if($programs) { ?>
-							<?php echo $programs->program_desc;?>
-							<br/>
-							<?php echo ($programs->budget->ps+$programs->budget->mooe+$programs->budget->co); ?>
-							
-						<?php } ?>	
-					<?php } ?>
+<form name="frm" method="POST">
+<div class="row">
+	<h3>New Appro</h3>
+	<div class="panel-group col-md-6" id="accordion1">
+		<?php foreach ($new_appro as $key => $code) { ?>
+			<div class="panel panel-default">
+
+			    <div class="panel-heading">
+			    <a data-toggle="collapse" data-parent="#accordion1" href="#collapse<?php echo $code->owner_code; ?>">
+			      <h4 class="panel-title">
+			        	
+						<b><?php  echo $code->department_desc." (".$code->department_code.")"; ?></b>
+						 - <?php  echo $code->owner_code; ?>
+					
+			      </h4>
+			      </a>
+			    </div>
+			    
+			<?php if($code->programs) { ?>
+				<div id="collapse<?php echo $code->owner_code; ?>" class="panel-collapse collapse">
+	      			<div class="panel-body">
+						<?php foreach ($code->programs as $key => $programs) { ?>
+							<?php if($programs) { ?>
+								<div class="row">
+									<div class="col-md-7">
+										<i class="glyphicon glyphicon-chevron-right"></i>&nbsp;&nbsp;<?php echo $programs->program_desc;?>
+									</div>
+									<div class="col-md-4">
+										Php <?php echo ($programs->budget->ps+$programs->budget->mooe+$programs->budget->co); ?>
+									</div>
+									<div class="col-md-1">
+										<input type="checkbox" name="data_new_appro[<?php echo $code->owner_code; ?>]" value="<?php echo $code->department_code."|".$code->owner_code."|".$programs->program_desc."|".($programs->budget->ps+$programs->budget->mooe+$programs->budget->co); ?>">
+									</div>
+								</div>
+							<?php } ?>	
+						<?php } ?>
+					</div>
 				</div>
+			<?php } ?>
 			</div>
 		<?php } ?>
-		</div>
-	<?php } ?>
+	</div>
+
+	<div class="col-md-4 col-md-offset-6" style="position:fixed !important;">
+	
+	</div>
+
 </div>
-<h3>Auto Appro</h3>	
-<div class="panel-group" id="accordion2">
-	<?php foreach ($auto_appro as $key => $code) { ?>
-		<div class="panel panel-default">
-		    <a data-toggle="collapse" data-parent="#accordion2" href="#collapse<?php echo $code->owner_code; ?>">
-		    <div class="panel-heading">
-		      <h4 class="panel-title">
-		        	
-					<b><?php  echo $code->department_desc." (".$code->department_code.")"; ?></b>
-			<!-- <td colspan=""><?php  echo $code->department_desc; ?> </td>  -->
-					 - <?php  echo $code->owner_code; ?>
-			<!-- <td colspan=""><?php  echo $code->owner_desc; ?> </td> --> 
-				
-		      </h4>
-		    </div>
-		    </a>
-		<?php if($code->programs) { ?>
-			<div id="collapse<?php echo $code->owner_code; ?>" class="panel-collapse collapse">
-      			<div class="panel-body">
-					<?php foreach ($code->programs as $key => $programs) { ?>
-						<?php if($programs) { ?>
-							<?php echo $programs->program_desc;?>
-							<br/>
-							<?php echo ($programs->budget->ps+$programs->budget->mooe+$programs->budget->co); ?>
-							
-						<?php } ?>	
-					<?php } ?>
+<div class="row">
+	<h3>Auto Appro</h3>	
+	<div class="panel-group col-md-6" id="accordion2">
+		<?php foreach ($auto_appro as $key => $code) { ?>
+			<div class="panel panel-default">
+
+			    <div class="panel-heading">
+			    <a data-toggle="collapse" data-parent="#accordion2" href="#collapse<?php echo $code->owner_code; ?>">
+			      <h4 class="panel-title">
+			        	
+						<b><?php  echo $code->department_desc." (".$code->department_code.")"; ?></b>
+						 - <?php  echo $code->owner_code; ?>
+					
+			      </h4>
+			      </a>
+			    </div>
+			    
+			<?php if($code->programs) { ?>
+				<div id="collapse<?php echo $code->owner_code; ?>" class="panel-collapse collapse">
+	      			<div class="panel-body">
+						<?php foreach ($code->programs as $key => $programs) { ?>
+							<?php if($programs) { ?>
+								<div class="row">
+									<div class="col-md-7">
+										<i class="glyphicon glyphicon-chevron-right"></i>&nbsp;&nbsp;<?php echo $programs->program_desc;?>
+									</div>
+									<div class="col-md-4">
+										Php <?php echo ($programs->budget->ps+$programs->budget->mooe+$programs->budget->co); ?>
+									</div>
+									<div class="col-md-1">
+										<input type="checkbox" name="data_auto_appro[<?php echo $code->owner_code; ?>]" value="<?php echo $code->department_code."|".$code->owner_code."|".$programs->program_desc."|".($programs->budget->ps+$programs->budget->mooe+$programs->budget->co); ?>">
+									</div>
+								</div>
+							<?php } ?>	
+						<?php } ?>
+					</div>
 				</div>
+			<?php } ?>
 			</div>
 		<?php } ?>
-		</div>
-	<?php } ?>
+	</div>
 </div>
+<input type="submit" name="diy_submit" value="send">
+</form>
 <script type="text/javascript">
 	$(document).ready(function(){
 		
