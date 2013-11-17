9d4ef05a75dcf64941c3accdba1452da2c8ee620
Ian Romie Ona <ianromie@gmail.com>
2013-11-16 17:38:38 -0800

    department controller

---
+++ protected/views/department/budget.php
@@ -0,0 +1,27 @@
+	
+<table class="table">
+	<?php foreach ($new_appro as $key => $code) { ?>
+		<tr class="toggle-programs">	
+			<td colspan=""><?php  echo $code->department_code; ?> </td> 
+			<td colspan=""><?php  echo $code->department_desc; ?> </td> 
+			<td colspan=""><?php  echo $code->owner_code; ?> </td> 
+			<td colspan=""><?php  echo $code->owner_desc; ?> </td> 
+		</tr>
+		<?php if($code->programs) { ?>
+			<?php foreach ($code->programs as $key => $programs) { ?>
+				<?php if($programs) { ?>
+					<tr class="toggle " >
+						<td width="100">-></td><td><?php echo $programs->program_desc;?></td><td><?php echo ($programs->budget->ps+$programs->budget->mooe+$programs->budget->co); ?></td>
+					</tr>	
+				<?php } ?>	
+			<?php } ?>
+		<?php } ?>
+	<?php } ?>
+</table>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.toggle-programs').click(function(){
+			$(this).closest("tr.toggle1").next().slideToggle();
+		})
+	})
+</script>
