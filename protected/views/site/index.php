ea130dc4c847c7878c2c9f9908d5efcf1b4bce1e
junereycasuga <junerey.casuga@gmail.com>
2013-11-17 09:43:25 +0800

    changes on the home page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -20,8 +20,8 @@ $this->pageTitle=Yii::app()->name;
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu sollicitudin nunc. Aliquam mauris mi, vulputate vitae elementum quis, varius et mauris. Ut erat lacus, pretium vel diam iaculis, imperdiet congue mi. Aenean vel est lacus. Morbi fermentum dapibus velit. Praesent in dui lobortis, tincidunt enim id, pellentesque augue. Curabitur in nunc luctus, scelerisque diam non, lobortis nunc. Aenean eleifend, risus non lacinia interdum, nunc ipsum varius velit, eget varius mauris magna id augue.
 					</p>
 					<div class="pull-right">
-						<a href="<?php echo $this->createAbsoluteUrl('department/'); ?>" class="btn btn-primary btn-lg">Budget Your Own</a>
-						<a href="<?php echo $this->createAbsoluteUrl('#'); ?>" class="btn btn-primary btn-lg"><i class="glyphicon glyphicon-thumbs-up"></i> Vote</a>
+						<a href="<?php echo $this->createAbsoluteUrl('department/budget'); ?>" class="btn btn-primary btn-lg">Make your own National Budget</a>
+						<a href="<?php echo $this->createAbsoluteUrl('#'); ?>" class="btn btn-primary btn-lg"><i class="glyphicon glyphicon-thumbs-up"></i> Vote other Budgets</a>
 					</div>
 				</div>
 			</div>	
