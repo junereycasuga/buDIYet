f8e9eb9e3c05c46d6e598eb6fbefc12a46ba34b1
Ian Romie Ona <ianromie@gmail.com>
2013-11-17 01:06:58 -0800

    ianromie

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -1,5 +1,16 @@
+<?php 
+$login = true;
+if($login){ ?>
+<style type="text/css">
+  .navbar {
+    display:none;
+  }
+</style>
 
+  <?php } ?>
+<center><h1 class="loading">Loading</h1></center>
 <div class="form">
+
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'login-form',
 	'enableClientValidation'=>true,
@@ -8,9 +19,8 @@
 	),
 )); ?>
 <div class="login">
-<div class="login">
     <div class="login-container"><img alt="Logo login@2x" height="30" src="images/logo-login_2x.png" width="100">
-      <form>
+      <form id="my_form">
         <div class="form-group">
           <div class="input-group"><span class="input-group-addon"><i class="icon-envelope"></i></span>
             <?php echo $form->textField($model,'username', array('class'=>'form-control','placeholder'=>"Username")); ?>
@@ -32,7 +42,22 @@
       </form>
       <a href="index.html#">Forgot password?</a></div>
   </div>	
-
+<script>
+  $(document).ready(function () {
+      var okToSubmit = false;
+      $('form').submit(function (e) {
+          if ( ! okToSubmit) {
+              e.preventDefault();
+              var $form = $(this);
+              setTimeout(function () {
+                  okToSubmit = true;
+                  $('.loading').fadeOut(1000).text("Authenticating").fadeIn(100);
+                  $form.submit();
+              }, 2000);
+          }
+      });
+  });
+</script>
 
 <?php $this->endWidget(); ?>
 </div><!-- form -->
